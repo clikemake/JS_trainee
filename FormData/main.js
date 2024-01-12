86a4b1094db572da888732ae221a535cb76b28df
Danila Harvonen <cppfl@yandex.ru>
2024-01-12 18:47:30 +0300

    Btn bugfix

--- FormData/main.js
+++ FormData/main.js
@@ -10,6 +10,7 @@ continue_btn.addEventListener('mouseenter', function() {
 
 continue_btn.addEventListener('mouseleave', function() {
     this.style.backgroundColor = "rgb(87, 0, 107)";
+    this.style.transform = "scale(1)";
 });
 
 continue_btn.addEventListener('mousedown', function() {
